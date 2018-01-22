751c105497c96a0fd06a002a75d683d9c75c8bf6
Raju Gandhi <raju.gandhi@gmail.com>
2018-01-22 18:52:14 -0500

    first few utils functions

---
+++ src/com/looselytyped/Utils.groovy
@@ -0,0 +1,16 @@
+#!/usr/bin/groovy
+package com.looselytyped
+
+def getBranch() {
+  def branch = env.BRANCH_NAME
+  if (!branch) {
+    try {
+      branch = sh(script: 'git symbolic-ref --short HEAD', returnStdout: true).toString().trim()
+    } catch (err) {
+      echo('Unable to get git branch and in a detached HEAD. You may need to select Pipeline additional behaviour and \'Check out to specific local branch\'')
+      return null
+    }
+  }
+  echo "Using branch ${branch}"
+  return branch
+}
